a70ff12ee17bcbc2c3da5b50308251ac4ed71d16
Chris Pelzer <chrispelzer@wayne.edu>
2018-09-17 16:44:27 -0400

    load extraPlugins that aren't part of the full CKeditor preset

--- config.js
+++ config.js
@@ -7,6 +7,9 @@ CKEDITOR.editorConfig = function( config ) {
     // Define changes to default configuration here. For example:
     // config.language = 'fr';
     // config.uiColor = '#AADC6E';
+
+    config.extraPlugins = 'buttonwayne,image2,youtube,youtubethumb'
+
     config.skin = 'moono';
     config.image2_altRequired = true;
     config.pasteFromWordRemoveFontStyles = true;
