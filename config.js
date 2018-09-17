54c56d3fd111fa03476126c2acf4d129c2c5d567
Chris Pelzer <chrispelzer@wayne.edu>
2018-09-17 15:03:04 -0400

    add custom config

--- config.js
+++ config.js
@@ -4,7 +4,126 @@
  */
 
 CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+    // Define changes to default configuration here. For example:
+    // config.language = 'fr';
+    // config.uiColor = '#AADC6E';
+    config.skin = 'moono';
+    config.image2_altRequired = true;
+    config.pasteFromWordRemoveFontStyles = true;
+
+    // ACF rules not allowed by any plugins
+    config.extraAllowedContent = '*(*)[data-*];' + // allow all classes and any data attribute on all elements
+        'iframe{*}[width,height,src,frameborder,allowfullscreen,title];' + // Don't require the attributes that the YouTube Plugin required
+        'img{margin*,padding*};' + // Allow margins and padding on <img> to be modifiable
+        'blockquote cite;' + // Allow <cite> to be within the <blockquote>
+        'dl dd dt;' // Allow <dl> <dd> and <dt> elements due to old Accordion code on older sites, can be removed in the future when not needed
+    ;
+
+    // Remove allowed margins when pasting from Word
+    this.on('afterPasteFromWord', function(evt){
+        var filter = evt.editor.filter.clone(),
+            fragment = CKEDITOR.htmlParser.fragment.fromHtml( evt.data.dataValue ),
+            writer = new CKEDITOR.htmlParser.basicWriter();
+
+        // Disallow certain styles.
+        filter.disallow( 'p{margin-*};*(Mso*)' );
+
+        // Process, and overwrite evt.data.dataValue.
+        filter.applyTo( fragment );
+        fragment.writeHtml( writer );
+        evt.data.dataValue = writer.getHtml();
+    });
+
+    this.on('instanceReady', function (evt){
+        evt.editor.filter.addTransformations([
+            [
+                {
+                    element: 'a',
+                    left: function(el) {
+                        var is_external = (el.attributes.hasOwnProperty('href') && el.attributes.href.substr(0, 4) === 'http' && el.attributes.target === '_blank');
+
+                        return is_external && !el.attributes.rel;
+                    },
+                    right: function(el){
+                        el.attributes.rel = 'noopener';
+                    }
+                }
+            ],
+            [
+                {
+                    element: 'img',
+                    left: function(el) {
+                        return Object.keys(el.styles).length === 0;
+                    },
+                    right: function(el){
+                        el.styles['padding'] = '10px';
+                    }
+                }
+            ],
+            [
+                {
+                    element: 'figure',
+                    left: function(el) {
+                        return Object.keys(el.styles).length === 0;
+                    },
+                    right: function(el){
+                        el.styles['padding-bottom'] = '10px';
+                    }
+                }
+            ]
+        ]);
+    });
+
+    config.toolbar_page = [
+        ['Source','-','Cut','Copy','Paste','PasteText','PasteFromWord','-','Print', 'SpellChecker', 'Scayt'],
+        ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+        ['Image','Table','HorizontalRule','SpecialChar','PageBreak'],
+        '/',
+        ['Bold','Italic','Strike','-','Subscript','Superscript'],
+        ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote','CreateDiv'],
+        ['JustifyLeft','JustifyCenter','JustifyRight'],
+        ['Link','Unlink','Anchor','Youtube'],
+        '/',
+        ['Format','FontSize'],
+        ['Maximize', 'ShowBlocks','-','About']
+    ];
+    config.toolbar_newsletter = [
+        ['Source','-','Cut','Copy','Paste','PasteText','PasteFromWord','-','Print', 'SpellChecker', 'Scayt'],
+        ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+        ['Image','Table','HorizontalRule','SpecialChar','PageBreak'],
+        '/',
+        ['Bold','Italic','Strike','-','Subscript','Superscript'],
+        ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote','CreateDiv'],
+        ['JustifyLeft','JustifyCenter','JustifyRight'],
+        ['Link','Unlink','Anchor','Youtube'],
+        '/',
+        ['Format','FontSize','TextColor'],
+        ['Maximize', 'ShowBlocks','-','About']
+    ];
+    config.toolbar_email = [
+        ['Source','-','Cut','Copy','Paste','PasteText','PasteFromWord','-','Print', 'SpellChecker', 'Scayt'],
+        ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+        ['Image','Table','HorizontalRule','SpecialChar','PageBreak'],
+        '/',
+        ['Bold','Italic','Strike','-','Subscript','Superscript'],
+        ['NumberedList','BulletedList','-'],
+        ['JustifyLeft','JustifyCenter','JustifyRight'],
+        ['Link','Unlink','Anchor','ButtonWayne','YoutubeThumb'],
+        '/',
+        ['Format','FontSize','TextColor'],
+        ['Maximize', 'ShowBlocks','-','About']
+    ];
+    config.toolbar_newsroom = [
+        ['Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo'],
+        ['Scayt'],
+        ['Link','Unlink','Anchor'],
+        ['Image','Table','HorizontalRule','SpecialChar'],
+        ['Maximize'],
+        ['Source'],
+        '/',
+        ['Bold','Italic','Strike','RemoveFormat','-','Subscript','Superscript'],
+        ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
+        ['Format'],
+        ['About']
+    ];
 };
