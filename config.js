a71d5a1f026817b160ff747f4d41feb4baec2d5f
Chris Pelzer <chrispelzer@gmail.com>
2025-05-23 08:59:28 -0400

    fix: update `BulletedList` spelling to include the missing `e`

--- config.js
+++ config.js
@@ -220,6 +220,6 @@ CKEDITOR.editorConfig = function( config ) {
         ['About']
     ];
     config.toolbar_forms = [
-        ['Bold','Italic','Underline','-','Strike','-','JustifyLeft','JustifyCenter','JustifyRight','BulletdList','NumberedList', 'Undo', 'Redo', 'Link', 'Unlink', 'Image', 'PasteText', 'RemoveFormat', 'Source'],
+        ['Bold','Italic','Underline','-','Strike','-','JustifyLeft','JustifyCenter','JustifyRight','BulletedList','NumberedList', 'Undo', 'Redo', 'Link', 'Unlink', 'Image', 'PasteText', 'RemoveFormat', 'Source'],
     ];
 };
